98fadd727b4f6827bd265f1135e86c0e88d733cb
Ashish Kumar Burnwal <ashishburnwal293@gmail.com>
2021-06-17 20:08:49 +0530

    Add urls

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
-const express = require("express");
+const express = require('express');
 
 const PORT = 8888;
 const app = express();
 
 const mount = () => {
-	app.listen(PORT);
-	console.log("Server is running at port 8888");
+  app.listen(PORT);
+  console.log('Server is running at port 8888');
 };
+
+app.use('/', (req, res) => {
+  res.send('Hello world');
+});
 
 mount(app);
